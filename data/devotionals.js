f59f0669aedb9f0e677c6442fe5ad3a4f4df8d8b
sambayemi <samuelobayemi070707@gmail.com>
2026-03-06 21:52:13 +0100

    Add initial devotionals data structure

---
+++ data/devotionals.js
@@ -0,0 +1,9 @@
+export default[
+{
+id:1,
+title:"Trust God's Guidance",
+scripture:"Psalm 23:1",
+message:"The Lord leads His people with care and provision.",
+prayer:"Lord guide my steps today."
+}
+]
